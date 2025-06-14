0fd75e5d2afab564506557abce467db20168bc45
Kartik220221 <kartikgupta2004@gmail.com>
2025-06-15 01:36:20 +0530

    origin url

--- src/Axios.jsx
+++ src/Axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"https://whatsapp-chat-analysis-backend.onrender.com",
+    baseURL:import.meta.env.VITE_BACKEND_URL ,
     withCredentials:true,
 })
